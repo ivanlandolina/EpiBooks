ca18ee2c8f85e800ba4f60c0e108ad154d4f78a1
Ivan Landolina <ivan.landolina99@gmail.com>
2025-08-06 00:11:38 +0200

    ulteriori modifiche effettuate

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,53 +1,66 @@
-import { Badge, Col } from "react-bootstrap";
+import { Badge, Col, Button } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 // import { useSelected } from "../context/selectedContext";
 import { Link } from "react-router";
-import { FcInfo } from "react-icons/fc";
+import { FaArrowRight } from "react-icons/fa";
 
 function SingleBook({ book, colorText, theme }) {
   // const { selected, setSelected } = useSelected();
 
   return (
     <Col sm={6} md={6} lg={4} xl={3} xxl={3} className="my-4">
-        <Card
-          className={
-            "h-100 " 
-            // +(selected == book.asin && "border  border-3 border-danger")
-          }
+      <Card
+        className={
+          "h-100 "
+          // +(selected == book.asin && "border  border-3 border-danger")
+        }
+      >
+        <Card.Img
+          // onClick={() =>
+          //   setSelected((oldAsin) => (book.asin == oldAsin ? "" : book.asin))
+          // }
+          variant="top"
+          src={book.img}
+          style={{ height: "370px", width: "100%", objectFit: "cover" }}
+        />
+        <Card.Body
+          style={{
+            backgroundColor: theme === "dark" ? "#1e1e1e" : "#ffffff",
+            color: colorText === "white" ? "#ffffff" : "#000000",
+          }}
         >
-          <Card.Img
-            // onClick={() =>
-            //   setSelected((oldAsin) => (book.asin == oldAsin ? "" : book.asin))
-            // }
-            variant="top"
-            src={book.img}
-            style={{ height: "370px", width: "100%", objectFit: "cover" }}
-          />
-          <Card.Body
+          <Card.Title
+            className="mb-3 fw-bold"
             style={{
-              backgroundColor: theme === "dark" ? "#1e1e1e" : "#ffffff",
-              color: colorText === "white" ? "#ffffff" : "#000000",
+              fontSize: "1.1rem",
+              lineHeight: "1.3",
+              height: "2.6rem",
+              overflow: "hidden",
+              display: "-webkit-box",
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: "vertical",
             }}
           >
-            <Card.Title
-              className="mb-2 fw-bold"
-              style={{
-                fontSize: "1.1rem",
-                lineHeight: "1.3",
-                height: "2.6rem",
-                overflow: "hidden",
-                display: "-webkit-box",
-                WebkitLineClamp: 2,
-                WebkitBoxOrient: "vertical",
-              }}
-            >
-              {book.title}
-            </Card.Title>
-            <Link to={"/books/" + book.asin}><FcInfo role="button" /></Link>
-            <Badge bg="secondary">€{book.price.toFixed(2)}</Badge>
-          </Card.Body>
-        </Card>
-      
+            {book.title}
+          </Card.Title>
+          <div className="d-flex justify-content-between align-items-center">
+            <Badge bg="warning" style={{ color: "black" }}>
+              €{book.price.toFixed(2)}
+            </Badge>
+            <Link to={"/books/" + book.asin} style={{ textDecoration: "none" }}>
+              <Button
+                style={{
+                  color: theme === "light" ? "black" : undefined,
+                }}
+                className="btnBook d-flex align-items-center"
+                role="button"
+              >
+                Dettagli <FaArrowRight style={{ fontSize: "12px" }} />
+              </Button>
+            </Link>
+          </div>
+        </Card.Body>
+      </Card>
     </Col>
   );
 }
--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -3,25 +3,23 @@ import { describe, expect, it } from "vitest";
 import App from "../App";
 
 describe("testiamo l'applicazione", () => {
-    it('renders welcome', ()=> {
-        render(<App />)
-        expect(screen.getByText(/EpiBooks ti offre un'esperienza di lettura smart/i)).toBeInTheDocument()
-    });
+  it("renderizza il componente Welcome", () => {
+    render(<App />);
+    expect(
+      screen.getByText(/EpiBooks ti offre un'esperienza di lettura smart/i)
+    ).toBeInTheDocument();
+  });
 
+  it("renderizza tante cards quanti sono i libri nel file json", () => {
+    render(<App />);
+    fireEvent.click(screen.queryByText("Home"));
+    expect(screen.getAllByRole("button")).toHaveLength(152);
+  });
 
-    it('renders five cards', ()=> {
-    
-    render(<App />)
-    fireEvent.click(screen.queryByText('Home'))
-    expect(screen.getAllByRole('button')).toHaveLength(154)
-
+  it("renderizza il componente CommentArea", async () => {
+    render(<App />);
+    fireEvent.click(screen.queryByText("Home"));
+    fireEvent.click(screen.getAllByRole("button")[0]);
+    await screen.findByText((text) => text.startsWith("Valutazione"))
+  });
 });
-
-it('renders comment area', async ()=> {
-    
-    render(<App />)
-    fireEvent.click(screen.queryByText('Home'))
-    fireEvent.click(screen.getAllByRole('button') [0])
-    expect(await screen.findByText(/Valutazione/i)).toBeInTheDocument()
-})
-})
